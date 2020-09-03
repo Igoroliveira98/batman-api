a42cc6c83e4130847c5df874f083fd7a9218c55f
Igor Oliveira <igoroliveira2798@gmail.com>
2020-09-03 17:52:31 -0300

    script

--- scripts/server.js
+++ scripts/server.js
@@ -10,8 +10,18 @@ app.use(cors());
 app.get("/", async (req, res) => {
 
     try{
-        let { data } = await axios("https://www.superheroapi.com/api.php/3246243112132664/106")
-        return res.json(data)
+        let { data } = await axios(`https://www.superheroapi.com/api.php/3246243112132664/70`)
+
+        const { powerstats, image, work, appearance } = data; 
+
+        const batman = {
+            powerstats,
+            image,
+            work,
+            appearance
+        }
+
+        return res.json(batman)
     } catch(error) {
         console.error(error);
     }
